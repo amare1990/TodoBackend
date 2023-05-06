61ee2d657e031309caf1b9807301cd91772eee07
amare1990 <amaremek@gmail.com>
2023-05-06 12:37:57 +0300

    Update the middleware for corsheaders

--- Todobackend/settings.py
+++ Todobackend/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
